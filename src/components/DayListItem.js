9861c0977c3c22f456b7052434215ccf08a08d4f
Sofiya Lipova <sofiya.lipova@mail.mcgill.ca>
2021-12-07 20:33:00 -0500

    set up the project

---
+++ src/components/DayListItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+
+  const formatSpots = () => {
+    return props.spots > 1 ? `${props.spots} spots remaining` : (props.spots === 1 ? "1 spot remaining" : "no spots remaining");
+  }
+
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots
+  });
+
+  return (
+    //console gets mad when unclickable days are clicked
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots()}</h3>
+    </li>
+  );
+}
