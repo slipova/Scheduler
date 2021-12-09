9206db77baeab0aecffd149247502b9d75084273
Sofiya Lipova <sofiya.lipova@mail.mcgill.ca>
2021-12-09 15:19:23 -0500

    display appoitments

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
+import Appointment from "components/Appointment";
+import Empty from "./Appointment/Empty";
 
 const days = [
   {
@@ -21,8 +23,53 @@ const days = [
   },
 ];
 
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
+
+
+
+
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const appointment = appointments.map(appointment => <Appointment key={appointment.id} {...appointment}></Appointment>)
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -48,7 +95,9 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {/* {appointments.map((appointment) => <Appointment key={appointment.id} {...appointment} />)} */}
+        {appointment}
+        <Empty time="5pm" />
       </section>
     </main>
   );
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,11 +6,18 @@ import Show from "./Show";
 import Empty from "./Empty";
 
 
+
 function Appointment(props) {
+  console.log(props)
   return (
     <>
+      <Header time={props.time} />
+
       {props.time && <article className="appointment">Appointment at {props.time}</article>}
       {!props.time && <article className="appointment">No appointments</article>}
+      {/* {props.interview ? <Show /> : <Empty />} */}
+      {props.interview && <Show interviewer={props.interview.interviewer} student={props.interview.student} />}
+      {!props.interview && <Empty />}
     </>
   );
 };
