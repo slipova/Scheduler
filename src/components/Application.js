6329d0c8c5ed3650eb6f2ae0e39735e83a5c845f
Sofiya Lipova <sofiya.lipova@mail.mcgill.ca>
2021-12-09 23:04:10 -0500

    connect to days database

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
 
 
 const appointments = [
@@ -67,7 +68,15 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
-  const schedule = appointments.map(appointment => <Appointment key={appointment.id} {...appointment}></Appointment>)
+  const [days, setDays] = useState([])
+  const schedule = appointments.map(appointment => <Appointment key={appointment.id} {...appointment}></Appointment>);
+
+  useEffect(() => {
+    axios.get("/api/days")
+      .then((response) => {
+        return setDays([...response.data])
+      });
+  }, []);
 
   return (
     <main className="layout">
@@ -94,7 +103,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {appointments.map((appointment) => <Appointment key={appointment.id} {...appointment} />)}
+        {/* {appointments.map((appointment) => <Appointment key={appointment.id} {...appointment} />)} */}
         {schedule}
         <Appointment time="5pm" key="last" />
       </section>
