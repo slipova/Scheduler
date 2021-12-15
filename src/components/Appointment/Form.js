f4e02a51d45ab970c67893ce85a618842515f7f6
slipova <sofiya.lipova@mail.mcgill.ca>
2021-12-15 00:27:16 -0500

    add name validation to form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,7 +6,16 @@ import Button from "components/Button";
 function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
+  function validate() {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+
+    props.onSave(student, interviewer);
+  }
 
 
   const reset = () => {
@@ -35,19 +44,19 @@ function Form(props) {
             onChange={(event) => setStudent(event.target.value)}
             data-testid="student-name-input"
           />
-
+          <section className="appointment__validation">{error}</section>
         </form>
+
         <InterviewerList
           interviewers={props.interviewers}
           value={interviewer}
           onChange={setInterviewer}
-
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={() => cancel()} >Cancel</Button>
-          <Button confirm onClick={() => props.onSave(student, interviewer)} >Save</Button>
+          <Button confirm onClick={validate} >Save</Button>
         </section>
       </section>
     </main>
