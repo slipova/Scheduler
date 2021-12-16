755624515b5984a208fa42774bdfaa285fe4d5af
slipova <sofiya.lipova@mail.mcgill.ca>
2021-12-15 22:59:26 -0500

    add tests for editing and deleting appointments(cypress)

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -4,7 +4,7 @@ describe("Navigation", () => {
   });
   it("should navigate to Tuesday", () => {
     cy.visit("/");
-    cy.contains("li", "Tuesday").click()
+    cy.contains("[data-testid=day]", "Tuesday").click()
       .should("have.class", "day-list__item--selected");
   });
 });
